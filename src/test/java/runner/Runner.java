68b1a16cd7f6269201a67dcae82aecc79cc05e81
Badrul_alam <badrulalam0123@gmail.com>
2023-04-03 22:39:31 +0600

    add code

--- src/test/java/runner/Runner.java
+++ src/test/java/runner/Runner.java
@@ -1,13 +1,14 @@
+
 package runner;
 
-import org.junit.runner.RunWith;
 
+import org.junit.runner.RunWith;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
  
 @RunWith(Cucumber.class)
 @CucumberOptions(
-		features = {"./src/test/resources/features"}, 
+		features = {"src/test/resources/features"}, 
 		glue = {"stepDefinitions"}, 
 		monochrome= true,
         plugin = {"pretty", "html:test-output", "json:target/cucumber-report/cucumber.json" }
